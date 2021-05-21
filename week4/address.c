85a1219767a61d9679f9eec8c10fdd9798116835
janham <janhampino@gmail.com>
2021-05-21 23:49:36 +0900

    add week4

---
+++ week4/address.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char *s = "HELLO!";
+    printf("%c\n", s[0]);
+    printf("%c\n", s[1]);
+    printf("%c\n", s[2]);
+    printf("%c\n", s[3]);
+    printf("%c\n", s[4]);
+    printf("%c\n", s[5]);
+}
